04f92c79ea545b75eec93a885935a2653c8b6908
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-05-04 20:21:42 -0300

    PROJETO FINALIZADO

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Jan 28, 2025 12:00:00").getTime();
+let launchDate = new Date("Dec 28, 2022 12:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
